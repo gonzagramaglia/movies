c8471ded0185f940b73c6aa8a42b66713be87929
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-25 05:31:55 -0300

    frontend: Header section. Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+"use client";
+
 import "./App.css";
+import { useState } from "react";
+import Search from "./components/Search";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
-      <main>
-        <div className="pattern"></div>
+      <main className="pattern">
         <div className="wrapper">
           <header>
+            <img src="./hero.png" alt="Hero Banner" />
             <h1>
               Find <span className="text-gradient">Movies</span> You&apos;ll
               Enjoy Without the Hassle
             </h1>
           </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <h1 className="text-white">{searchTerm}</h1>
         </div>
       </main>
     </>
